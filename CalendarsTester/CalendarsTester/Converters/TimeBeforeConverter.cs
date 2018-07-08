7033d2866860278bcf42289ae822cb509450c310
Caleb Clarke <TheAlmightyBob@users.noreply.github.com>
2018-07-07 23:57:31 -0700

    Added Humanizer for friendlier display of reminders

---
+++ CalendarsTester/CalendarsTester/Converters/TimeBeforeConverter.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CalendarsTester.Converters
+{
+    public class TimeBeforeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan ts)
+            {
+                return $"{ts.Humanize(5)} before";
+            }
+
+            return $"Invalid input: {value}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
